81e1a16fe16966673b95c81c9800c4edb90339cf
Lorenzo Trevizani <lorenzotrevizani@Lorenzos-Laptop.local>
2024-07-14 18:02:12 -0300

    Create start screen of app

---
+++ lib/Texts/quiz_main_title.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QuizMainTitle extends StatelessWidget {
+  const QuizMainTitle(this.text, {super.key});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 28,
+        color: Colors.white,
+      ),
+    );
+  }
+}

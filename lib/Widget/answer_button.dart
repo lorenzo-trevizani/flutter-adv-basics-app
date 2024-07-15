a3d687806b034d1c696ab067f7c267e7aaccd94a
Lorenzo Trevizani <lorenzotrevizani@Lorenzos-Laptop.local>
2024-07-15 18:01:45 -0300

    Create dynamic question screen

---
+++ lib/Widget/answer_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.answerText, this.onTap, {super.key});
+
+  final String answerText;
+  final void Function() onTap; 
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 40,
+          vertical: 10,
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:adv_basics/Widget/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
@@ -12,8 +15,28 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Center(
-      child: Text('Questions Screen'),
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(
+              answer,
+              () {},
+            );
+          }),
+        ],
+      ),
     );
   }
 }
